f347672d2a6d139cd264f4a5740f794df34b390c
namvh19 <vuhainam321123@gmail.com>
2020-11-01 14:46:14 +0700

    upvideo
    upvideo

--- server/src/routers/router.js
+++ server/src/routers/router.js
@@ -47,7 +47,7 @@ router.post('/course/create', courseController.createCourse);
   // });
 
 
-const userController = require('../controller/user.controller');
+/*const userController = require('../controller/user.controller');
 const videoController = require('../controller/video.controller')
 const auth = require('../middleware/auth');
 const isLecture = require("../middleware/checkIsLecture");
@@ -55,9 +55,9 @@ const isLecture = require("../middleware/checkIsLecture");
 router.post('/user', userController.createUser);
 router.post('/login', userController.login);
 router.get('/token', auth, userController.checkToken)
-
+*/
 // test check is lecture
-router.get('/test', auth, isLecture, videoController.createFile)
+//router.get('/test', auth, isLecture, videoController.createFile)
 
 
 module.exports = router;
